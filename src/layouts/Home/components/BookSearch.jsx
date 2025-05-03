a3ff708b1caa6142609929086e013cc36b510f39
Juan.fong <juan.fong@bitsoluciones.com.co>
2025-05-03 11:54:59 -0500

    se agrega el filtro de isbn

--- src/layouts/Home/components/BookSearch.jsx
+++ src/layouts/Home/components/BookSearch.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import { Button, Input } from "antd";
+import { RetweetOutlined } from "@ant-design/icons";
 
-const BookSearch = ({ handleSearch, resetFilters }) => {
+const BookSearch = ({ handleSearch, resetFilters, selectField }) => {
   return (
     <div style={{ marginBottom: 20 }}>
       <Input.Search
-        placeholder="Buscar nombre"
+        placeholder="Escribe lo que buscas"
         onSearch={handleSearch} // Llama a handleSearch cuando se realiza una búsqueda
         enterButton
+        disabled={!selectField}
         style={{ width: "100%" }}
       />
 
-      <Button onClick={resetFilters} block style={{marginTop: 10}}>Reiniciar Filtros</Button>
+      <Button onClick={resetFilters} block style={{marginTop: 10}} icon={<RetweetOutlined />}>Reiniciar Filtros</Button>
     </div>
   );
 };
